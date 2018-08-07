20625c02d0606c4ccdcc9ccb3ccec6c9508fc461
Luis Takahashi <luis.takahashi@liveecommerce.com.br>
2018-08-07 12:49:55 -0300

    Adicionado estilizações base + smoth Scroll + polyfills

--- js/index.js
+++ js/index.js
@@ -2,18 +2,37 @@ gama = [];
 
 gama.initialize = function () {
     gama.setup();
+    gama.scrollListener();
 };
 
 gama.setup = function () {
     // Firebase setup
     try {
-        let app = firebase.app();
-        let features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function');
+        var app = firebase.app();
+        var features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function');
         console.log(`Firebase SDK carregado com sucesso!  Recursos: ${features.join(', ')}`);
     } catch (e) {
         console.error(e);
     }
 }
+
+gama.scrollListener = function () {
+    var anchorSelector = '[href^="#"]';
+    $(document).on('click', anchorSelector, function (e) {
+        e.preventDefault();
+
+        var header =  $('header');
+        var selector = $(this).attr('href');
+        var element = selector == '#' ? header : $(selector);
+        var height = selector == '#' ? 0 : element.offset().top + header.outerHeight();
+
+        window.scroll({
+            top: height , 
+            behavior: 'smooth' 
+        });
+    })
+}
+
 // Activates when dom is loaded
 $(function () {
     gama.initialize();
